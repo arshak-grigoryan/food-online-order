a1fc62df4be44b8b65682737b04eac8a911c023c
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-12-01 19:12:35 +0400

    add header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import SelectOptions from '../selectOptions/SelectOptions';
+import Search from '../search/Search';
+import ShoppingCart from '../shoppingCart/ShoppingCart';
+import './header.scss';
+
+const Header = () => {
+
+    return (
+        <div className='header'>
+            <div className='left'></div>
+            <div className='searchFilter'>
+                <SelectOptions/>
+                <Search/>                
+            </div>
+            <div className='cart'>
+                <ShoppingCart/>                   
+            </div>
+        </div>
+    )
+}
+
+export default Header
