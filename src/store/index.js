b9059b8f73d691023011f570697220850e20a5d9
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-12-01 19:04:33 +0400

    add store

---
+++ src/store/index.js
@@ -0,0 +1,7 @@
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import shopReducer from './reducers';
+
+const store = createStore(shopReducer, composeWithDevTools())
+
+export default store
