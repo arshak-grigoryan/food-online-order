929ec45519a7e3c552f5fc1a304ff1e4fdd6962a
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-12-01 20:24:28 +0400

    add kitchenType filter

--- src/components/restaurants/Restaurants.jsx
+++ src/components/restaurants/Restaurants.jsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useFetch } from "../../hooks";
 import { RESTAURANTS_URL } from "../../urls";
-import { getRestaurants, getSearchedName } from '../../store/selectors';
+import { getRestaurants, getSearchedName, getSelectedKitchenType } from '../../store/selectors';
 import { addRestaurants } from "../../store/actions";
 import RestaurantCard from '../restaurantCard/RestaurantCard';
-import { useDispatch, useSelector } from 'react-redux';
 import './restaurants.css';
 
 const Restaurants = () => {
@@ -12,24 +12,35 @@ const Restaurants = () => {
 
     const restaurantsData = useFetch(RESTAURANTS_URL)
 
-    const { restaurants, searchedName } = useSelector((state) => ({
-        restaurants: getRestaurants(state),
-        searchedName: getSearchedName(state),
-    }))
+    const {
+        restaurants,
+        searchedName,
+        selectedKitchenType,
+    } = useSelector(
+        (state) => ({
+            restaurants: getRestaurants(state),
+            searchedName: getSearchedName(state),
+            selectedKitchenType: getSelectedKitchenType(state),
+        })
+    )
 
     useEffect(() => {
-        console.log(restaurantsData)
         dispatch(addRestaurants(restaurantsData))
-    },[restaurantsData])
+    },[dispatch, restaurantsData])
 
     return (
         <div className='restaurants'>
             {
-                restaurants.map(({id, name, ...restaurant}, i) => {
-                    const lowercasedName = name.toLowerCase()
-                    if(lowercasedName.includes(searchedName)) {
-                        return <RestaurantCard key={id} id={id} name={name} {...restaurant}/>
-                    }
+                restaurants
+                .filter(({ kitchenTypes }) => {
+                    return (selectedKitchenType === 'all' || kitchenTypes.includes(selectedKitchenType))
+                })
+                .filter(({ name }) => {
+                    const lowercasedName = name.toLowerCase();
+                    return lowercasedName.includes(searchedName)
+                })
+                .map(({ id, ...props }) => {
+                    return <RestaurantCard key={id} id={id} {...props}/>
                 })
             }
         </div>
