f16cc0953606e53488858f79983810170f8588b1
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-12-01 19:04:05 +0400

    add actions

---
+++ src/store/actions/index.js
@@ -0,0 +1,46 @@
+import {
+    ADD_ITEM_BASKET,
+    DELETE_ITEM_BASKET,
+    INCREASE_ITEM,
+    DECREASE_ITEM,
+    ADD_RESTAURANTS,
+    ADD_SEARCH_NAME
+} from '../constants';
+
+export const addItemBasket = () => {
+    return {
+        type: ADD_ITEM_BASKET,
+    }
+}
+
+export const deleteItemBasket = () => {
+    return {
+        type: DELETE_ITEM_BASKET,
+    }
+}
+
+export const increaseItem = () => {
+    return {
+        type: INCREASE_ITEM,
+    }
+}
+
+export const decreaseItem = () => {
+    return {
+        type: DECREASE_ITEM,
+    }
+}
+
+export const addRestaurants = (restaurants) => {
+    return {
+        type: ADD_RESTAURANTS,
+        payload: { restaurants }
+    }
+}
+
+export const addSearchName = (name) => {
+    return {
+        type: ADD_SEARCH_NAME,
+        payload: { name }
+    }
+}
