babc2d4e525f3a09f887fdb940883e889a63b4ad
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-12-01 22:18:02 +0400

    add restaurant route

--- src/components/router/Router.jsx
+++ src/components/router/Router.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import NotAuthorized from '../pages/notAuthorized/NotAuthorized';
 import SuspensedRoute from './SuspensedRoute';
 import Home from '../pages/home/Home';
+import Restaurant from '../pages/restaurant/Restaurant';
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Switch>
                 <SuspensedRoute exact path='/restaurants' component={Home}/>
+                <SuspensedRoute path='/restaurants/:id' component={Restaurant}/>
                 <Route path='/notAuthorized'component={NotAuthorized}/>
                 <Redirect exact from='/' to='/restaurants'/> 
                 <Redirect to='/notAuthorized'/>
