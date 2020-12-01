7140dcb85ecbd174a8e468b8119d4091f2d539de
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-12-01 19:17:29 +0400

    add app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from './components/pages/home/Home';
+import './app.css';
+
+const App = () => {
+    return (
+        <div className='app'>
+            <Home/>
+        </div>
+    )
+}
+
+export default App
