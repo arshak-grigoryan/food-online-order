54df27801fc0d5e3b81a18ddf0395c6712b106b0
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-12-01 19:02:35 +0400

    add selector

---
+++ src/store/selectors/index.js
@@ -0,0 +1,4 @@
+export const getKitchenTypes = (state) => state.kitchenTypes;
+export const getRestaurants = (state) => state.restaurants;
+export const getBasket = (state) => state.basket;
+export const getSearchedName = (state) => state.searchedName;
