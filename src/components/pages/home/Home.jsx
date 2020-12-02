af8c35a3fedd8e3e363dadd7679b8cf47878caf2
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-12-02 17:07:18 +0400

    add basket visibility

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,14 +1,22 @@
+import { useSelector } from 'react-redux';
+import { getBasketVisibility } from '../../../store/selectors';
 import Header from '../../header/Header';
 import Restaurants from '../../restaurants/Restaurants';
+import Basket from '../../basket/Basket';
 import './home.scss';
 
 const Home = () => {
 
+    const { basketVisibility } = useSelector((state) => ( { basketVisibility: getBasketVisibility(state) } ))
+
     return (
+        <>
+        { basketVisibility && <Basket/> }
         <div className='home'>
             <Header isSelectOptionExist={true} placeholder='Search Restaurants'/>
             <Restaurants/>
         </div>
+        </>
     )
 }
 
