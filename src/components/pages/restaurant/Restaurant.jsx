6410dfbb1d9e1694e03763ddcb188832db484a54
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-12-02 14:04:02 +0400

    add filter by menuItem name and by kitchenType

--- src/components/pages/restaurant/Restaurant.jsx
+++ src/components/pages/restaurant/Restaurant.jsx
@@ -1,14 +1,29 @@
-import { useEffect, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { useFetch } from '../../../hooks';
+import { useFetch, useMount } from '../../../hooks';
+import { RESTAURANTS_URL } from "../../../urls";
+import { addSearchName } from '../../../store/actions';
+import { getSearchedName } from '../../../store/selectors';
 import MenuItem from '../../menuItem/MenuItem';
+import Search from '../../search/Search';
 import './restaurant.scss';
 
 const Restaurant = () => {
-
+    const [selectedKitchenTypes, setSelectedKitchenTypes] = useState(null)
+    const [currentRestaurant, setCurrentRestaurant] = useState(null)
+    const dispatch = useDispatch()
     const [menuItems, setMenuItems] = useState([])
     const { id } = useParams()
     const menu = useFetch(`/mock/menus/${id}.json`)
+    const restaurants = useFetch(RESTAURANTS_URL)
+
+    const { searchedName } = useSelector((state) => ( { searchedName: getSearchedName(state) } ))
+
+    useMount(() => {
+        // reset search value for current page when page visited for first time reloaded
+        dispatch(addSearchName(''))
+    })
 
     useEffect(() => {
         if (menu) {
@@ -16,14 +31,76 @@ const Restaurant = () => {
         }
     }, [menu])
 
+    useEffect(() => {
+        if(restaurants) {
+            const currentRestaurant = restaurants.find((restaurant) => restaurant.id === Number(id))
+            if(currentRestaurant) {
+                setCurrentRestaurant(currentRestaurant)
+                setSelectedKitchenTypes(currentRestaurant.kitchenTypes)
+            }
+        }
+    }, [restaurants, id])
+
+    const onCheckboxChoose = (e) => {
+        const chechked = e.target.checked
+        const value = e.target.value
+        if(selectedKitchenTypes.includes(value) && chechked) {
+            setSelectedKitchenTypes([value])
+        }
+        else if(!selectedKitchenTypes.includes(value) && chechked) {
+            setSelectedKitchenTypes((prevTypes) => {
+                return [...prevTypes, value]
+            })
+        }
+        else if(selectedKitchenTypes.includes(value) && chechked === false) {
+            setSelectedKitchenTypes((prevTypes) => {
+                return prevTypes.filter((type) => type !== value)
+            })
+        }
+    }
+
     return (
         <div className='restaurant'>
-            {
-                menuItems.map(({id, ...props}) => {
-                    return <MenuItem key={id} {...props}/>
-                })
-            }
+            <Search placeholder='Search Menu Items'/>
+            <div className='menuWrapper'>
+                <div className='menuFilter'>
+                    <h2>Kitchen Types</h2>
+                    {
+                        currentRestaurant && currentRestaurant.kitchenTypes.map((kitchen, i) => {
+                            return (
+                                <div className='chechkboxItem' key={i}>
+                                    <input type="checkbox" id={kitchen} name={kitchen} value={kitchen} onChange={(e) => onCheckboxChoose(e)}/>
+                                    <label htmlFor={kitchen}>{kitchen}</label>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+                <div className='restaurantMenu'>
+                    {
+                        menuItems
+                            .filter(({ name }) => {
+                                return name.toLowerCase().includes(searchedName)
+                            })
+                            .filter(({ kitchenType }) => {
+                                if(selectedKitchenTypes) {
+                                    if(selectedKitchenTypes.length) {
+                                        return selectedKitchenTypes.includes(kitchenType)
+                                    }
+                                    return true
+                                } else {
+                                    return true
+                                }
+                            })
+                            .map(({ id, ...props }) => {
+                                return <MenuItem key={id} {...props}/>
+                            })
+                    }
+                </div>                   
+            </div>
+         
         </div>
+
     )
 }
 
