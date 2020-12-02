731039ef0977077852cc43bfa03c91d7d4343e16
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-12-02 17:03:41 +0400

    add totalPrice

--- src/components/basket/Basket.jsx
+++ src/components/basket/Basket.jsx
@@ -1,22 +1,38 @@
-import { useDispatch } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { closeBasket } from "../../store/actions";
+import { getBasket } from '../../store/selectors';
 import { CLASS_NAMES } from '../../constants';
 import Icon from '../icon/Icon';
 import './basket.scss';
 
 const Basket = () => {
+    const [totalPrice, setTotalPrice] = useState(0)
+
     const dispatch = useDispatch()
+    const { basket } = useSelector((state) => ({ basket: getBasket(state) }))
 
     const onCloseBasketClick = () => {
         dispatch(closeBasket())
     }
+
+    useEffect(() => {
+        console.log(basket)
+        if(basket) {
+            let total = 0;
+            basket.forEach(({price, count}) => {
+                total += price * count
+            });
+            setTotalPrice(total)
+        }
+    },[basket])
 
     return (    
         <div className='basketRight'>
             <div className='top'>
                 <div className='left'></div>
                 <div className='totalPrice'>
-                    {/* <h3>Total {totalPrice}</h3> */}
+                    <h3>Total {totalPrice}</h3>
                 </div>
                 <div className='closeBasket'onClick={onCloseBasketClick}>
                     <Icon type={CLASS_NAMES.close}/>
