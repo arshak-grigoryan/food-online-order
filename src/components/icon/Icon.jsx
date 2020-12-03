5ce54b1c6b461e9b77c9d5408f2280c2ef173fba
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-12-04 01:56:37 +0400

    icon changes

--- src/components/icon/Icon.jsx
+++ src/components/icon/Icon.jsx
@@ -1,8 +1,12 @@
 import React from "react";
-import './icon.css'
+import "./icon.scss";
 
-function Icon({type, ...props}) {
-  return <i className={type} {...props}></i>
+function Icon({ type, style = {} }) {
+  return (
+    <div className="faicon">
+      <i className={type} style={style}></i>
+    </div>
+  );
 }
 
 export default Icon;
