4f7504bc07749fd5234ad07142fac5e67bc045ef
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-12-04 02:13:12 +0400

    selectOption changes

--- src/components/selectOptions/SelectOptions.jsx
+++ src/components/selectOptions/SelectOptions.jsx
@@ -1,36 +1,39 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch } from "react-redux";
 import { useFetch, useMount } from "../../hooks";
-import { KITCHEN_TYPES_URL } from "../../urls";
-import { selectKitchenType } from '../../store/actions';
+import { KITCHEN_TYPES_URL } from "../../constants";
+import { selectKitchenType } from "../../store/actions";
 
-import './selectOptions.scss';
+import "./selectOptions.scss";
 
 const SelectOptions = () => {
-    const dispatch = useDispatch()
-    
-    const kitchenTypes = useFetch(KITCHEN_TYPES_URL)
+  const dispatch = useDispatch();
 
-    const onSelectChange = (e) => {
-        dispatch(selectKitchenType(e.target.value))
-    }
+  const kitchenTypes = useFetch(KITCHEN_TYPES_URL);
 
-    useMount(() => {
-        // reset selected option for current page when page visited for first time reloaded
-        dispatch(selectKitchenType('all'))
-    })
+  const onSelectChange = (e) => {
+    dispatch(selectKitchenType(e.target.value));
+  };
 
-    return (
-        <div className='selectOptions'>
-            <select id="kitchens" onChange={(e) => onSelectChange(e)} >
-                <option value='all'>All</option>
-                {
-                    kitchenTypes && kitchenTypes.map((kitchen, i) => {
-                        return <option value={kitchen.abbr} key={i}>{kitchen.name}</option>
-                    })
-                }
-            </select>
-        </div>
-    )
-}
+  useMount(() => {
+    // reset selected option for current page when page visited for first time reloaded
+    dispatch(selectKitchenType("all"));
+  });
 
-export default SelectOptions
+  return (
+    <div className="selectOptions">
+      <select id="kitchens" onChange={(e) => onSelectChange(e)}>
+        <option value="all">All</option>
+        {kitchenTypes &&
+          kitchenTypes.map((kitchen, i) => {
+            return (
+              <option value={kitchen.abbr} key={i}>
+                {kitchen.name}
+              </option>
+            );
+          })}
+      </select>
+    </div>
+  );
+};
+
+export default SelectOptions;
