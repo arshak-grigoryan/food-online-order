cb895d2abd6ee7557451f86c505c60b948e2b55d
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-12-04 02:18:45 +0400

    header changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,34 +1,45 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
-import { CLASS_NAMES } from '../../constants';
-import SelectOptions from '../selectOptions/SelectOptions';
-import Search from '../search/Search';
-import ShoppingCart from '../shoppingCart/ShoppingCart';
-import Icon from '../icon/Icon';
-import './header.scss';
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { CLASS_NAMES } from "../../constants";
+import SelectOptions from "../selectOptions/SelectOptions";
+import Search from "../search/Search";
+import ShoppingCart from "../shoppingCart/ShoppingCart";
+import Icon from "../icon/Icon";
+import "./header.scss";
 
-const Header = ({isSelectOptionExist=false, placeholder='search', isBackExist=false}) => {
+const Header = ({
+  isSelectOptionExist = false,
+  placeholder,
+  isBackExist = false,
+  isRestaurantsSearch,
+}) => {
+  const history = useHistory();
 
-    const history = useHistory()
+  const onGoBackClick = () => {
+    history.push("/");
+  };
 
-    const onGoBackClick = () => {
-        history.push('/')
-    }
+  return (
+    <div className="header">
+      <div className="left">
+        {isBackExist && (
+          <button onClick={onGoBackClick}>
+            <Icon type={CLASS_NAMES.arrowLeft} style={{ color: "#2c3e50" }} />
+          </button>
+        )}
+      </div>
+      <div className="searchFilter">
+        {isSelectOptionExist && <SelectOptions />}
+        <Search
+          placeholder={placeholder}
+          isRestaurantsSearch={isRestaurantsSearch}
+        />
+      </div>
+      <div className="cart">
+        <ShoppingCart />
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div className='header'>
-            <div className='left'>
-                { isBackExist && <button onClick={onGoBackClick}><Icon type={CLASS_NAMES.arrowLeft}/></button> }
-            </div>
-            <div className='searchFilter'>
-                { isSelectOptionExist && <SelectOptions/> }
-                <Search placeholder={placeholder}/>                
-            </div>
-            <div className='cart'>
-                <ShoppingCart/>                   
-            </div>
-        </div>
-    )
-}
-
-export default Header
+export default Header;
