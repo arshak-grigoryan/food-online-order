d6fe9756f803f4eec85b8d61f462b4b1ece4856f
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-12-04 01:58:24 +0400

    shoppingCart chnages

--- src/components/shoppingCart/ShoppingCart.jsx
+++ src/components/shoppingCart/ShoppingCart.jsx
@@ -1,22 +1,21 @@
-import { useDispatch } from 'react-redux';
-import { openBasket } from '../../store/actions';
-import { CLASS_NAMES } from '../../constants'
-import Icon from '../icon/Icon';
-import './shoppingCart.scss';
+import { useDispatch } from "react-redux";
+import { openBasket } from "../../store/actions";
+import { CLASS_NAMES } from "../../constants";
+import Icon from "../icon/Icon";
+import "./shoppingCart.scss";
 
 const ShoppingCart = () => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  const onOpenBasketClick = () => {
+    dispatch(openBasket());
+  };
 
-    const onOpenBasketClick = () => {
-        dispatch(openBasket())
-    }
+  return (
+    <div className="shoppingCart" onClick={onOpenBasketClick}>
+      <Icon type={CLASS_NAMES.shoppingCart} style={{ fontSize: "20px" }} />
+    </div>
+  );
+};
 
-    return (
-        <div className='shoppingCart' onClick={onOpenBasketClick}>
-            <Icon type={CLASS_NAMES.shoppingCart}/>
-        </div>
-    )
-}
-
-export default ShoppingCart
+export default ShoppingCart;
