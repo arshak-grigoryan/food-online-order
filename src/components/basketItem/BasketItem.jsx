2ea061f27f4af7fa7852125b6164196e60dc0291
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-12-04 02:20:37 +0400

    basketItem changes

--- src/components/basketItem/BasketItem.jsx
+++ src/components/basketItem/BasketItem.jsx
@@ -1,64 +1,61 @@
-import { useDispatch } from 'react-redux';
-import { CLASS_NAMES } from '../../constants';
-import { deleteItemBasket, decreaseItem, increaseItem } from "../../store/actions";
-import Icon from '../icon/Icon';
-import './basketItem.scss';
+import { useDispatch } from "react-redux";
+import { CLASS_NAMES } from "../../constants";
+import {
+  deleteItemBasket,
+  decreaseItem,
+  increaseItem,
+} from "../../store/actions";
+import Icon from "../icon/Icon";
+import "./basketItem.scss";
 
-const BasketItem = ({
-    id,
-    name,
-    photoUrl,
-    price,
-    count,
-    ruiid
-}) => {
-    const dispatch = useDispatch()
+const BasketItem = ({ id, name, photoUrl, price, count, ruiid }) => {
+  const dispatch = useDispatch();
 
-    const onBasketItemDeleteClick = () => {
-        dispatch(deleteItemBasket(ruiid))
-    }
+  const onBasketItemDeleteClick = () => {
+    dispatch(deleteItemBasket(ruiid));
+  };
 
-    const onBasketItemDecreaseClick = () => {
-        dispatch(decreaseItem(ruiid))
-    }
-    
-    const onBasketItemIncreaseClick = () => {
-        dispatch(increaseItem(ruiid))
-    }
+  const onBasketItemDecreaseClick = () => {
+    dispatch(decreaseItem(ruiid));
+  };
 
-    return (
-        <div className='basketItem'>
-            <div className='nameDeleteItem'>
-                <div className='name'>
-                    <h3>{name}</h3>
-                </div>
-                <div className='deleteItem' onClick={onBasketItemDeleteClick}>
-                    <Icon type={CLASS_NAMES.close}/>
-                </div>                
-            </div>
-            <div className='wrapper'>
-                <div className='photo'>
-                    <img src={photoUrl} alt="img"/>
-                </div>
-                <div className='priceCounter'>
-                    <div className='price'>
-                        <h3>{price}</h3>
-                    </div>
-                    <div className='counter'>
-                        <div className='decrease' onClick={onBasketItemDecreaseClick}>
-                            <p>-</p>
-                        </div>
-                        <div className='currentCount'>
-                            <p>{count}</p>
-                        </div>
-                        <div className='increase' onClick={onBasketItemIncreaseClick}>
-                            <p>+</p>
-                        </div>
-                    </div>
-                </div>                
-            </div>
+  const onBasketItemIncreaseClick = () => {
+    dispatch(increaseItem(ruiid));
+  };
+
+  return (
+    <div className="basketItem">
+      <div className="nameDeleteItem">
+        <div className="name">
+          <h3>{name}</h3>
         </div>
-    )
-}
+        <div className="deleteItem" onClick={onBasketItemDeleteClick}>
+          <Icon type={CLASS_NAMES.close} />
+        </div>
+      </div>
+      <div className="wrapper">
+        <div className="photo">
+          <img src={photoUrl} alt="img" />
+        </div>
+        <div className="priceCounter">
+          <div className="price">
+            <h3>{price}</h3>
+          </div>
+          <div className="counter">
+            <div className="decrease" onClick={onBasketItemDecreaseClick}>
+              <Icon type={CLASS_NAMES.minus} />
+            </div>
+            <div className="currentCount">
+              <p>{count}</p>
+            </div>
+            <div className="increase" onClick={onBasketItemIncreaseClick}>
+              <Icon type={CLASS_NAMES.plus} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default BasketItem
+export default BasketItem;
