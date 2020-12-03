6030a800860c460f651c6b04240a2605fce87eb2
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-12-04 02:16:49 +0400

    home changes

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,23 +1,46 @@
-import { useSelector } from 'react-redux';
-import { getBasketVisibility } from '../../../store/selectors';
-import Header from '../../header/Header';
-import Restaurants from '../../restaurants/Restaurants';
-import Basket from '../../basket/Basket';
-import './home.scss';
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getBasketVisibility,
+  getIsBasketAnimation,
+} from "../../../store/selectors";
+import { animateBasket } from "../../../store/actions";
+import Header from "../../header/Header";
+import Restaurants from "../../restaurants/Restaurants";
+import Basket from "../../basket/Basket";
+import "./home.scss";
+import { useEffect } from "react";
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const { basketVisibility, isBasketAnimation } = useSelector((state) => ({
+    basketVisibility: getBasketVisibility(state),
+    isBasketAnimation: getIsBasketAnimation(state),
+  }));
 
-    const { basketVisibility } = useSelector((state) => ( { basketVisibility: getBasketVisibility(state) } ))
+  useEffect(() => {
+    if (basketVisibility) {
+      setTimeout(() => dispatch(animateBasket()));
+    }
+  }, [basketVisibility]);
 
-    return (
-        <>
-        { basketVisibility && <Basket/> }
-        <div className='home'>
-            <Header isSelectOptionExist={true} placeholder='Search Restaurants'/>
-            <Restaurants/>
-        </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div
+        className="home"
+        style={{ width: isBasketAnimation ? "calc(100% - 300px)" : "100%" }}
+      >
+        <Header
+          isSelectOptionExist={true}
+          placeholder="Search Restaurants"
+          isRestaurantsSearch={true}
+        />
+        <Restaurants />
+      </div>
+      {basketVisibility && (
+        <Basket style={{ right: isBasketAnimation ? "0" : "-300px" }} />
+      )}
+    </>
+  );
+};
 
-export default Home
+export default Home;
