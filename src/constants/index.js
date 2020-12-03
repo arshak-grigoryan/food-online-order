7ea1b49576cf5a59c41e0717e4e6e19532c79b2e
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-12-04 01:59:57 +0400

    add constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,4 +2,8 @@ export const CLASS_NAMES = {
     shoppingCart: 'fas fa-shopping-cart',
     close: 'fas fa-times',
     arrowLeft: 'fas fa-arrow-left',
+    minus: 'fas fa-minus',
+    plus: 'fas fa-plus',
 }
+export const RESTAURANTS_URL = '/mock/restaurants.json';
+export const KITCHEN_TYPES_URL = '/mock/kitchenTypes.json';
