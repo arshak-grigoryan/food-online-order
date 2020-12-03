d450dd9c6373c0af1c0c68c8680b61ad0633a50b
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-12-04 02:28:00 +0400

    restaurantCard changes

--- src/components/restaurantCard/RestaurantCard.jsx
+++ src/components/restaurantCard/RestaurantCard.jsx
@@ -1,35 +1,31 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
-import './restaurantCard.scss';
+import React from "react";
+import { useHistory } from "react-router-dom";
+import "./restaurantCard.scss";
 
-const RestaurantCard = ({
-    id,
-    name,
-    photoUrl,
-    kitchenTypes
-}) => {
-    const history = useHistory()
-    
-    const onRestaurantClick = () => {
-        history.push(`/restaurants/${id}`)
-    }
+const RestaurantCard = ({ id, name, photoUrl, kitchenTypes }) => {
+  const history = useHistory();
 
-    return (
-        <div className='restaurantCard' onClick={onRestaurantClick}>
-            <img src={photoUrl} alt="img"/>
-            <h2>{name}</h2>
-            <div className='kitchens'>
-                <h3>Kitchens</h3>
-                <div className='kitchenTypes'>
-                    {
-                        kitchenTypes.map((kitchen, i) => {
-                            return <p key={i}>{kitchen}</p>
-                        })
-                    }                    
-                </div>
-            </div>
+  const onRestaurantClick = () => {
+    history.push(`/restaurants/${id}`);
+  };
+
+  return (
+    <div className="restaurantCardWrapper">
+      <div className="restaurantCard" onClick={onRestaurantClick}>
+        <img src={photoUrl} alt="img" />
+        <h2>{name}</h2>
+        <div className="kitchenTypes">
+          {kitchenTypes.map((kitchen, i) => {
+            return (
+              <div className="kitchenWrapper" key={i}>
+                <div className="kitchen">{kitchen}</div>
+              </div>
+            );
+          })}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default RestaurantCard
+export default RestaurantCard;
