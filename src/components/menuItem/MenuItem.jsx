2818e733793a1a2fa221f808e44139701571530d
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-12-04 12:51:47 +0400

    menuItem changes

--- src/components/menuItem/MenuItem.jsx
+++ src/components/menuItem/MenuItem.jsx
@@ -1,34 +1,35 @@
-import { useDispatch } from 'react-redux';
-import { CLASS_NAMES } from '../../constants';
-import { addItemBasket } from '../../store/actions';
-import Icon from '../icon/Icon';
-import './menuItem.scss';
+import { useDispatch } from "react-redux";
+import { CLASS_NAMES } from "../../constants";
+import { addItemBasket } from "../../store/actions";
+import Icon from "../icon/Icon";
+import "./menuItem.scss";
 
-const MenuItem = ({
-    id,
-    ruiid,
-    name,
-    photoUrl,
-    price,
-    kitchenType
-}) => {
-    const dispatch = useDispatch()
+const MenuItem = ({ id, ruiid, name, photoUrl, price, kitchenType }) => {
+  const dispatch = useDispatch();
 
-    const onAddItemBasketClick = () => {
-        dispatch(addItemBasket({ id, name, photoUrl, price, ruiid }))
-    }
+  const onAddItemBasketClick = () => {
+    dispatch(addItemBasket({ id, name, photoUrl, price, ruiid }));
+  };
 
-    return (
-        <div className='menuItem'>
-            <img src={photoUrl} alt="img"/>
-            <h2>{name}</h2>
-            <p>Price {price}</p>
-            <p>Kiitchen {kitchenType}</p>
-            <div className='basket' onClick={onAddItemBasketClick}>
-                <Icon type={CLASS_NAMES.shoppingCart}/>
-            </div>
+  return (
+    <div className="menuItemWrapper">
+      <div className="menuItem">
+        <img src={photoUrl} alt="img" />
+        <h2>{name}</h2>
+        {/* <div className='kitchenTypes'>
+                    <div className='kitchenWrapper'>
+                        <div className='kitchen'>{kitchenType}</div>
+                    </div>
+                </div> */}
+        <div className="priceBasket">
+          <div className="price">$ {price}</div>
+          <div className="basket" onClick={onAddItemBasketClick}>
+            <Icon type={CLASS_NAMES.shoppingCart} />
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default MenuItem
+export default MenuItem;
