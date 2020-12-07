ba94152c7bd8a5a4b40690a4ff19bf12badbcb51
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-12-07 18:02:25 +0400

    add modal

---
+++ src/components/modal/Modal.jsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import ReactDOM from "react-dom";
+
+import "./modal.scss";
+
+const modalRoot = document.getElementById("modal");
+
+const Modal = ({ children }) => {
+  const el = document.createElement("div");
+
+  useEffect(() => {
+    modalRoot.appendChild(el);
+    return () => modalRoot.removeChild(el);
+  }, [children]);
+
+  return ReactDOM.createPortal(children, el);
+};
+
+export default Modal;
