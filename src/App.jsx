2160456c2533727d0b5b9c7bebafceb692a676bb
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-12-07 18:06:39 +0400

    add modal in the root

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
-import Router from './components/router/Router';
-import './app.css';
+import React from "react";
+import { useSelector } from "react-redux";
+import { getOrderModalVisibility } from "./store/selectors";
+import Router from "./components/router/Router";
+import Modal from "./components/modal/Modal";
+import Order from "./components/popup/order/Order";
+import "./app.css";
 
 const App = () => {
-    return (
-        <div className='app'>
-            <Router/>
-        </div>
-    )
-}
+  const { isOrderModal } = useSelector((state) => ({
+    isOrderModal: getOrderModalVisibility(state),
+  }));
 
-export default App
+  return (
+    <div className="app">
+      <Router />
+      <Modal>{isOrderModal && <Order />}</Modal>
+    </div>
+  );
+};
+
+export default App;
