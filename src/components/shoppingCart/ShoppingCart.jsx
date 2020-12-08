8c9c5228002f641772c29939e1e2f2a843d8660c
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-12-09 02:33:47 +0400

    shoppingCart changes

--- src/components/shoppingCart/ShoppingCart.jsx
+++ src/components/shoppingCart/ShoppingCart.jsx
@@ -1,21 +1,39 @@
-import { useDispatch } from "react-redux";
-import { openBasket } from "../../store/actions";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { openCart, startCartOpeningAnimation } from "../../store/actions";
+import { getCart } from "../../store/selectors";
 import { CLASS_NAMES } from "../../constants";
 import Icon from "../icon/Icon";
 import "./shoppingCart.scss";
 
 const ShoppingCart = () => {
+  const [leng, setLeng] = useState();
+
   const dispatch = useDispatch();
 
-  const onOpenBasketClick = () => {
-    dispatch(openBasket());
+  const { cart } = useSelector((state) => ({
+    cart: getCart(state),
+  }));
+
+  useEffect(() => {
+    let counter = 0;
+    cart.forEach(({ count }) => {
+      counter += count;
+    });
+    setLeng(counter);
+  }, [cart]);
+
+  const onOpenCartClick = () => {
+    dispatch(openCart());
+    setTimeout(() => dispatch(startCartOpeningAnimation()));
   };
 
-  return (
-    <div className="shoppingCart" onClick={onOpenBasketClick}>
+  return leng ? (
+    <div className="shoppingCart" onClick={onOpenCartClick}>
       <Icon type={CLASS_NAMES.shoppingCart} style={{ fontSize: "20px" }} />
+      <sup>{leng}</sup>
     </div>
-  );
+  ) : null;
 };
 
 export default ShoppingCart;
