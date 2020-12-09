a2fedc4377e583bf3ef01cdd8e4b70b4291d7552
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-12-09 15:58:13 +0400

    add CART_WIDTH,MONTHS,TRANSITION_TIME_MS,DAYS

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,8 @@
+// URL
+export const RESTAURANTS_URL = "/mock/restaurants.json";
+export const CUISINES_URL = "/mock/cuisines.json";
+
+// STYLE
 export const CLASS_NAMES = {
   shoppingCart: "fas fa-shopping-cart",
   close: "fas fa-times",
@@ -5,6 +10,30 @@ export const CLASS_NAMES = {
   minus: "fas fa-minus",
   plus: "fas fa-plus",
 };
-export const RESTAURANTS_URL = "/mock/restaurants.json";
-export const CUISINES_URL = "/mock/cuisines.json";
 export const TRANSITION_TIME_MS = 300;
+export const CART_WIDTH = "300px";
+
+// OTHER
+export const MONTHS = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+export const DAYS = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
