edb12fe6bbeb420acaf85d6c765a4925816894a5
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-12-09 17:24:38 +0400

    delete unnecessary parts

--- src/components/restaurantItem/RestaurantItem.jsx
+++ src/components/restaurantItem/RestaurantItem.jsx
@@ -1,11 +1,8 @@
 import React from "react";
-import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
-import {} from "../../store/actions";
 import "./restaurantItem.scss";
 
 const RestaurantItem = ({ id, name, photoUrl, cuisines }) => {
-  const dispatch = useDispatch();
   const history = useHistory();
 
   const onRestaurantClick = () => {
