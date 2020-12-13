10ddcce3bd19fe76dfdaafe5dc86b5df70932063
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-12-14 02:14:20 +0400

    .

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,17 @@
 module.exports = {
-  "extends": [
+  extends: [
     "airbnb/base",
     "react-app"
   ],
-  "rules": {
-    // ruleValue ? ok : warn
+  rules: {
     "quotes": ["warn", "single"],
-    "linebreak-style": "warn",
-    "arrow-body-style": "warn",
-    "object-curly-newline": "warn",
-    "comma-dangle": "warn",
-    "operator-linebreak": "warn",
+    "max-len": "off",
+    "linebreak-style": "off", // prettier endOfLine
     "no-plusplus": "off",
-    // https://eslint.org/docs/rules/no-param-reassign#disallow-reassignment-of-function-parameters-no-param-reassign
-    // "no-param-reassign": ["warn", { "props": false }],
     "no-param-reassign": "off",
-    // https://eslint.org/docs/rules/object-curly-spacing#enforce-consistent-spacing-inside-braces-object-curly-spacing
     "import/no-extraneous-dependencies": ["warn", { "devDependencies": true }],
   },
-  "overrides": [
+  overrides: [
     {
       "files": [
         "**/*.ts?(x)"
@@ -28,7 +21,7 @@ module.exports = {
       }
     }
   ],
-  "settings" : {
+  settings : {
     // https://stackoverflow.com/questions/42264007/eslint-errorring-importing-jsx-without-extension
     "import/resolver": {
       "node": {
@@ -37,3 +30,57 @@ module.exports = {
     }
   }
 }
+
+
+
+// module.exports = {
+//   extends: [
+//     "airbnb/base",
+//     "react-app"
+//   ],
+//   rules: {
+//     "quotes": ["warn", "single"],
+//     "no-multiple-empty-lines": "warn",
+//     "no-trailing-spaces": "warn",
+//     "arrow-body-style": "warn", // if line length is bigger than max arrow will have return 
+//     "object-curly-newline": ["warn", {
+//       "ObjectExpression": { "multiline": true, "minProperties": 1 },
+//       "ObjectPattern": { "multiline": true },
+//       "ImportDeclaration": "never",
+//       "ExportDeclaration": "never"
+//     }],
+//     "operator-linebreak": "warn",
+//     "comma-dangle": ["error", "only-multiline"],
+//     // "max-len": ["error", {
+//     //   "code": 80,
+//     //   "ignoreComments": true
+//     // }], // prettier printWidth
+//     "max-len": "off",
+//     "linebreak-style": "off", // prettier endOfLine
+//     "no-plusplus": "off",
+//     // "indent": "off", // pretiier Tabs
+//     // https://eslint.org/docs/rules/no-param-reassign#disallow-reassignment-of-function-parameters-no-param-reassign
+//     // "no-param-reassign": ["warn", { "props": false }],
+//     "no-param-reassign": "off",
+//     // https://eslint.org/docs/rules/object-curly-spacing#enforce-consistent-spacing-inside-braces-object-curly-spacing
+//     "import/no-extraneous-dependencies": ["warn", { "devDependencies": true }],
+//   },
+//   overrides: [
+//     {
+//       "files": [
+//         "**/*.ts?(x)"
+//       ],
+//       "rules": {
+//         "additional-typescript-only-rule": "warn"
+//       }
+//     }
+//   ],
+//   settings : {
+//     // https://stackoverflow.com/questions/42264007/eslint-errorring-importing-jsx-without-extension
+//     "import/resolver": {
+//       "node": {
+//         "extensions": [".js",".jsx"]
+//       }
+//     }
+//   }
+// }
