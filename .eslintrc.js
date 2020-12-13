92640a9cd860f3b8c21593c733d4d944eedf47cb
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-12-13 20:43:24 +0400

    change eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,22 @@
 module.exports = {
   "extends": [
+    "airbnb/base",
     "react-app"
   ],
   "rules": {
-    "quotes": ["warn", "single"]
+    // ruleValue ? ok : warn
+    "quotes": ["warn", "single"],
+    "linebreak-style": "warn",
+    "arrow-body-style": "warn",
+    "object-curly-newline": "warn",
+    "comma-dangle": "warn",
+    "operator-linebreak": "warn",
+    "no-plusplus": "off",
+    // https://eslint.org/docs/rules/no-param-reassign#disallow-reassignment-of-function-parameters-no-param-reassign
+    // "no-param-reassign": ["warn", { "props": false }],
+    "no-param-reassign": "off",
+    // https://eslint.org/docs/rules/object-curly-spacing#enforce-consistent-spacing-inside-braces-object-curly-spacing
+    "import/no-extraneous-dependencies": ["warn", { "devDependencies": true }],
   },
   "overrides": [
     {
@@ -14,5 +27,13 @@ module.exports = {
         "additional-typescript-only-rule": "warn"
       }
     }
-  ]
+  ],
+  "settings" : {
+    // https://stackoverflow.com/questions/42264007/eslint-errorring-importing-jsx-without-extension
+    "import/resolver": {
+      "node": {
+        "extensions": [".js",".jsx"]
+      }
+    }
+  }
 }
