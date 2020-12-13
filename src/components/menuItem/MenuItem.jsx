175bb9950e79f133861377a2fe2865f041d96f47
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-12-13 18:16:57 +0400

    change quotes

--- src/components/menuItem/MenuItem.jsx
+++ src/components/menuItem/MenuItem.jsx
@@ -1,27 +1,39 @@
-import { useDispatch } from 'react-redux';
-import { CLASS_NAMES } from '../../constants';
-import { addCartItem } from '../../store/actions';
+import {
+  useDispatch,
+} from 'react-redux';
+import {
+  CLASS_NAMES,
+} from '../../constants';
+import {
+  addCartItem,
+} from '../../store/actions';
 import Icon from '../icon/Icon';
 import './menuItem.scss';
 
-const MenuItem = ({ id, ruiid, name, photoUrl, price }) => {
+const MenuItem = ({
+  id, ruiid, name, photoUrl, price,
+},) => {
   const dispatch = useDispatch();
 
   const onaddCartItemick = () => {
-    dispatch(addCartItem({ id, name, photoUrl, price, ruiid }));
+    dispatch(addCartItem({
+      id, name, photoUrl, price, ruiid,
+    },),);
   };
 
   return (
-    <div className='menuItemWrapper'>
-      <div className='menuItem'>
+    <div className="menuItemWrapper">
+      <div className="menuItem">
         <div
-          className='photo'
-          style={{ backgroundImage: `url(${photoUrl})` }}
+          className="photo"
+          style={{
+            backgroundImage: `url(${photoUrl})`,
+          }}
         ></div>
         <h2>{name}</h2>
-        <div className='priceCart'>
-          <div className='price'>$ {price}</div>
-          <div className='cart' onClick={onaddCartItemick}>
+        <div className="priceCart">
+          <div className="price">$ {price}</div>
+          <div className="cart" onClick={onaddCartItemick}>
             <Icon type={CLASS_NAMES.shoppingCart} />
           </div>
         </div>
