5b82e49a2e1c955ee637d64e1152fb9d4c911965
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-12-14 00:40:07 +0400

    eslint fix

--- src/components/icon/Icon.jsx
+++ src/components/icon/Icon.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import './icon.scss';
 
-function Icon({
-  type, style = {
-  }
-}) {
+function Icon({ type, style = {} }) {
   return (
     <div className="faicon">
       <i className={type} style={style}></i>
